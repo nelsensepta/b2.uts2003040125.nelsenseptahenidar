774908dae51466b00fe3f8c1a3c5465b4533017a
Nelsen Septa <nelsenseptahenidar@gmail.com>
2022-11-04 17:37:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/b2uts2003040125nelsenseptahenidar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.b2uts2003040125nelsenseptahenidar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
